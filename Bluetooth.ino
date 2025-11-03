89efb55efcc5a8d92a5ae069284a59c98d53d8d7
La Quý Trung Nguyên <dabest0123456@gmail.com>
2025-11-03 22:53:54 +0700

    Add files via upload

---
+++ Bluetooth.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(10, 11);
+
+void setup() {
+  pinMode(9,OUTPUT);
+  digitalWrite(9,HIGH);
+
+  Serial.begin(38400);
+  Serial.println("Enter AT Commands: ");
+  BTSerial.begin(38400); // HC-05 default speed in AT command mode
+  
+}
+
+void loop() {
+  if(Serial.available())
+    BTSerial.write(Serial.read());
+
+  if (BTSerial.available())
+    Serial.write(BTSerial.read());
+    
+  
+
+}
